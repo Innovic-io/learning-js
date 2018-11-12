ef3ed60910ef6b4d742a85688c08637be2f85123
Bane <branislav.simsic@innovic.io>
2018-11-12 09:55:11 +0100

    http call with request

--- request.js
+++ request.js
@@ -1,14 +1,12 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const request = require('request');
 
-const request = new XMLHttpRequest();
 
-const url = 'http://www.recipepuppy.com/api/?oi=1&q=milk+cheese+salt&p=1';
-
-request.open("GET", url);
-
-request.send();
-
-request.onreadystatechange = () => {
-    if(request.readyState === 4)
-    console.log(request.responseText);
-}
+request('http://www.recipepuppy.com/api/?oi=1&q=milk+cheese+salt&p=1', {json: true}, (error, response, body) => {
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log('status Code: ', response && response.statusCode);
+        console.log('Body: ', body);
+    }
+});
