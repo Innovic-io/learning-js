2d4b0c5679ea4535c5900b1725559bfaf55c015f
Bane <branislav.simsic@innovic.io>
2018-11-14 10:41:00 +0100

    some little changes

--- 8_func_as_arg.js
+++ 8_func_as_arg.js
@@ -9,3 +9,30 @@ function Goodbye()
 }
 
 Hello(Goodbye());
+
+
+this.a = 'a';
+
+function Person (name) {
+    this.name = name;
+
+    console.log(this.a);
+    return (callback, a, b) => {
+
+        return callback(a, b);
+    }
+}
+
+function dummyCallback(a, b) {
+    return a + b
+}
+
+const myCopy = dummyCallback;
+const a = (a, b) => {
+    console.log(this.a);
+    return a + b;
+}
+console.log(a(2,2))
+const p = new Person('a')(myCopy, 2, 3);
+
+console.log(p)
